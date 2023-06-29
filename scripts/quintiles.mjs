c833152882b3999509f179e611e6c10bbc74dd59
elvery.simon@abc.net.au <simon@elvery.net>
2023-06-29 14:15:52 +1000

    Update data processing pipeline

--- scripts/quintiles.mjs
+++ scripts/quintiles.mjs
@@ -3,12 +3,14 @@ import xlsx from 'xlsx';
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import { writeFile } from 'node:fs';
+import { readFileSync, writeFile } from 'node:fs';
+import { csvParse } from 'd3-dsv';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const dataPath = path.join(__dirname, '..', 'data');
 const inputPath = path.join(dataPath, 'ben-philips-housing-data', 'Rent Mortgage Oz2.xls');
+const tenurePath = path.join(dataPath, 'ben-philips-housing-data', 'tenureqntl.csv');
 const outputPath = path.join(dataPath, 'housing-data-clean', 'quintiles.json');
 
 const workbook = xlsx.readFile(inputPath);
@@ -21,12 +23,23 @@ const clean = data.reduce((acc, d) => {
   let q = d.Year;
   Object.entries(d).forEach(([key, value]) => {
     if (key !== 'Year') {
-      acc.push([q, +key, value]);
+      acc.push(['overall', q, +key, value]);
     }
   });
   return acc;
 }, []);
 
-writeFile(outputPath, JSON.stringify(clean), err => {
-  if (err) throw err;
-});
+const byTenure = csvParse(readFileSync(tenurePath).toString());
+
+writeFile(
+  outputPath,
+  JSON.stringify([
+    ...clean,
+    ...byTenure.map(d => {
+      return [d.Tenure === 'Renter' ? 'rent' : 'mortgage', d.Quintile, +d.Year, +d.Mortgage];
+    })
+  ]),
+  err => {
+    if (err) throw err;
+  }
+);
